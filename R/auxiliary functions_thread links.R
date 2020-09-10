b82a66ac96b7ee0b42b53ac7d57fe9e04b7a2c3d
fellennert <felix.lennert@icloud.com>
2020-09-10 14:20:25 +0200

    updated functions

--- R/auxiliary functions_thread links.R
+++ R/auxiliary functions_thread links.R
@@ -1,25 +1,61 @@
 # auxiliary functions -- scraping thread links ----------------------------
 
-# get n_pages
+# get pages
 
-get_n_pages <- function(page) {
-  n_threads <- rvest::html_nodes(page, ".threads strong") %>%
-    rvest::html_text() %>%
-    as.numeric()
-  n_pages <- floor(n_threads/20+1)
-  return(n_pages)
+scrape_section_pages <- function(section_link) {
+  base_link <- stringr::str_sub(section_link, end=-2)
+  pages <- vector(mode = "list", length = 10000000L)
+  i <- 1
+  indicator <- TRUE
+
+  while (indicator == TRUE) {
+    pages[[i]] <- xml2::read_html(paste0(base_link, "-", i, "/"))
+    indicator <- pages[[i]] %>%
+      rvest::html_nodes(".forumListing-thread a") %>%
+      rvest::html_text() %>%
+      length(.) > 0
+    i <- i + 1
+  }
+
+  return(pages %>% purrr::compact())
 }
 
-# build links within section
+### w/ cut-off
 
-build_links_section <- function(suffix, n_pages) {
-  n <- 1:n_pages
-  temp_link <- stringr::str_sub(suffix, end=-2)
-  links <- character(length = length(n))
-  for (i in seq_along(n)) {
-    links[i] <- paste0("http://gamla.familjeliv.se", temp_link, "-", n[i], "/")
+scrape_section_pages_w_cutoff <- function(section_link, cut_off, type) {
+  cut_off_date <- lubridate::ymd(cut_off)
+  base_link <- stringr::str_sub(section_link, end=-2)
+  pages <- vector(mode = "list", length = 10000000L)
+  i <- 1
+  indicator <- TRUE
+  indicator_date <- lubridate::today()
+
+  if (type == "start_date") {
+    while (cut_off_date <= indicator_date & indicator == TRUE) {
+      pages[[i]] <- xml2::read_html(paste0(base_link, "-", i, "/"))
+      indicator <- pages[[i]] %>%
+        rvest::html_nodes(".forumListing-thread a") %>%
+        rvest::html_text() %>%
+        length(.) > 0
+      indicator_date <- get_start_date(pages[[i]]) %>%
+        min(., na.rm = TRUE)
+      i <- i + 1
+    }
   }
-  return(links)
+
+  if (type == "latest_entry") {
+    while (cut_off_date <= indicator_date & indicator == TRUE) {
+      pages[[i]] <- xml2::read_html(paste0(base_link, "-", i, "/"))
+      indicator <- pages[[i]] %>%
+        rvest::html_nodes(".forumListing-thread a") %>%
+        rvest::html_text() %>%
+        length(.) > 0
+      indicator_date <- get_latest_entry(pages[[i]]) %>%
+        min(., na.rm = TRUE)
+      i <- i + 1
+    }
+  }
+  return(pages %>% purrr::compact())
 }
 
 # get threads' links
@@ -43,7 +79,10 @@ get_start_date <- function(page) {
     rvest::html_text()
   for (i in seq_along(start_date)) {
     if (stringr::str_detect(start_date[[i]], "Idag") == TRUE) start_date[[i]] <- as.character(lubridate::today())
+    if (is.na(start_date[[i]]) & i > 1) start_date[[i]] <- start_date[[i-1]]
+    if (is.na(start_date[[i]]) & i == 1) start_date[[i]] <- start_date[[i+1]]
   }
+
   return(lubridate::ymd(start_date))
 }
 
@@ -54,6 +93,8 @@ get_latest_entry <- function(page) {
     rvest::html_text()
   for (i in seq_along(latest_entry)) {
     if (stringr::str_detect(latest_entry[[i]], "Idag") == TRUE) latest_entry[[i]] <- as.character(lubridate::today())
+    if (is.na(latest_entry[[i]]) & i > 1) latest_entry[[i]] <- latest_entry[[i-1]]
+    if (is.na(latest_entry[[i]]) & i == 1) latest_entry[[i]] <- latest_entry[[i+1]]
     if (stringr::str_detect(latest_entry[[i]], "Obesvarat") == TRUE) latest_entry[[i]] <- NA_character_
   }
   return(lubridate::ymd(latest_entry))
--- R/get_threads.R
+++ R/get_threads.R
@@ -1,54 +1,66 @@
-#' Get threads
+#' get_threads
 #' Returns the thread links
 #'
 #' @param suffix A character string. The section's suffix.
+#' @param return_dates A logical vector. If \code{TRUE}, start date and date of
+#' latest entry are returned.
 #' @param thread_start_date A character string of a date in the format
 #' "YYYY-MM-DD". Links to threads which were started before this date will not
-#' be returned. Defaults to "2000-01-01".
-#' @param latest_entry A character string of a date in the format "YYYY-MM-DD".
-#' Only links to threads whose latest entry was posted on or after this very
-#' date are returned.
+#' be returned.
+#' @param latest_entry_date A character string of a date in the format
+#' "YYYY-MM-DD". Only links to threads whose latest entry was posted on or after
+#' this very date are returned.
 #'
-#' @return A tibble with three columns. \code{link} contains the thread's link,
-#' \code{start_date} the date it was created on, and \code{latest_entry} the
-#' date when the latest posting was made within the thread.
+#' @return A tibble with two columns: \code{thread_link} contains the thread's
+#' link, \code{thread_name} the thread name/title. If \code{return_dates} is set
+#' to \code{TRUE}, or cut-off dates are provided, \code{start_date} -- the date
+#' the thread was created on, and \code{latest_entry}, the date when the latest
+#' posting was added to the thread, are returned as well.
 #'
 #' @examples
-#' get_threads(suffix = "/Forum-19-89/", thread_start_date = "2020-04-01", latest_entry = "2020-05-01")
+#' get_threads(suffix = "/Forum-19-89/", return_dates = TRUE, thread_start_date = "2020-04-01", latest_entry_date = "2020-05-01")
 #'
 #' @export
-get_threads <- function(suffix, thread_start_date = "2000-01-01", latest_entry = "2000-01-01") {
-  page <- xml2::read_html(paste0("http://gamla.familjeliv.se", suffix))
-  n_pages <- get_n_pages(page)
-  links <- build_links_section(suffix, n_pages)
+get_threads <- function(suffix, return_dates = FALSE, thread_start_date = NULL, latest_entry_date = NULL) {
+  section_link <- paste0("http://gamla.familjeliv.se", suffix)
 
-  i <- 0
+  if (is.null(thread_start_date) == TRUE & is.null(latest_entry_date) == TRUE) pages <- scrape_section_pages(section_link)
 
-  thread_links <- vector("list", length = n_pages)
-  thread_names <- vector("list", length = n_pages)
-  thread_start_dates <- vector("list", length = n_pages)
-  latest_entry_dates <- vector("list", length = n_pages)
-  date_ind <- lubridate::today()
-
-  while ((date_ind >= lubridate::ymd(thread_start_date)) &&
-         (i < length(links))) {
-    i <- i + 1
-    page <- xml2::read_html(links[[i]])
-
-    thread_links[[i]] <- get_thread_links(page)
-    thread_names[[i]] <- get_thread_names(page)
-    thread_start_dates[[i]] <- get_start_date(page)
-    latest_entry_dates[[i]] <- get_latest_entry(page)
-
-    date_ind <- lubridate::ymd(tail(latest_entry_dates[[i]], 1))#
-    if (is.na(date_ind) == TRUE) date_ind <- lubridate::ymd(tail(thread_start_dates[[i]], 1))
+  if (is.null(thread_start_date) == TRUE & is.null(latest_entry_date) == TRUE & return_dates == FALSE) {
+    return(tibble::tibble(
+      thread_link = purrr::map(pages, get_thread_links) %>% purrr::reduce(c),
+      thread_name = purrr::map(pages, get_thread_names) %>% purrr::reduce(c)
+    ))
   }
-  return(tibble::tibble(
-    link = thread_links %>% purrr::compact() %>% purrr::reduce(c),
-    name = thread_names %>% purrr::compact() %>% purrr::reduce(c),
-    start_date = thread_start_dates %>% purrr::compact() %>% purrr::reduce(c),
-    latest_entry = latest_entry_dates %>% purrr::compact() %>% purrr::reduce(c)
+
+  if (is.null(thread_start_date) == TRUE & is.null(latest_entry_date) == TRUE & return_dates == TRUE) {
+    return(tibble::tibble(
+      thread_link = purrr::map(pages, get_thread_links) %>% purrr::reduce(c),
+      thread_name = purrr::map(pages, get_thread_names) %>% purrr::reduce(c),
+      start_date = purrr::map(pages, get_start_date) %>% purrr::reduce(c),
+      latest_entry = purrr::map(pages, get_latest_entry) %>% purrr::reduce(c)
+    ))
+  }
+
+  if (is.null(latest_entry_date) == FALSE) {
+    pages <- scrape_section_pages_w_cutoff(section_link, cut_off = latest_entry_date, type = "latest_entry")
+    return(tibble::tibble(
+      thread_link = purrr::map(pages, get_thread_links) %>% purrr::reduce(c),
+      thread_name = purrr::map(pages, get_thread_names) %>% purrr::reduce(c),
+      start_date = purrr::map(pages, get_start_date) %>% purrr::reduce(c),
+      latest_entry = purrr::map(pages, get_latest_entry) %>% purrr::reduce(c)
     ) %>%
-      dplyr::filter(start_date >= lubridate::ymd(thread_start_date))
-  )
+      dplyr::filter(latest_entry >= latest_entry_date))
+  }
+
+  if (is.null(thread_start_date) == FALSE) {
+    pages <- scrape_section_pages_w_cutoff(section_link, cut_off = thread_start_date, type = "start_date")
+    return(tibble::tibble(
+      thread_link = purrr::map(pages, get_thread_links) %>% purrr::reduce(c),
+      thread_name = purrr::map(pages, get_thread_names) %>% purrr::reduce(c),
+      start_date = purrr::map(pages, get_start_date) %>% purrr::reduce(c),
+      latest_entry = purrr::map(pages, get_latest_entry) %>% purrr::reduce(c)
+    ) %>%
+      dplyr::filter(start_date >= thread_start_date))
+  }
 }
